309148ed8693662e1b7f78edc11e433948ca8c2b
jetese <jetese2@gmail.com>
2019-02-14 17:47:20 +0100

    Simulación de hojas de árbol con muelles

---
+++ Physics/PracticaHojasArbol/Assets/Scripts/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    #region InEditorVariables
+
+    public float Mass;
+
+    #endregion
+    //public float Mass;
+    public bool Fixed;
+
+    public Vector3 Pos;
+    public Vector3 Vel;
+    public Vector3 Force;
+
+    public MassSpringSolid Manager;
+    public Node(float masa, Vector3 posicion, bool fijado)
+    {
+        Mass = masa;
+        Pos = posicion;
+        Fixed = fijado;
+    }
+	
+    public void ActualizarPos(Vector3 posicion)
+    {
+        Pos = posicion;
+    }
+	// Update is called once per frame
+	public Vector3 ObtenerPos() {
+        return Pos;
+	}
+
+    public void AddForces(float alpha)
+    {
+        Force += Mass * Manager.Gravity;
+        Force -= Mass * alpha * Vel;
+    }
+
+    public void AddForce(Vector3 fuerza)
+    {
+        Force += fuerza;
+    }
+
+    public void AddMass(float m)
+    {
+        Mass += m;
+    }
+}
