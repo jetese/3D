777534b8abf87184a1ceda2605c7dda0a559137f
jetese <jetese2@gmail.com>
2019-02-14 17:43:22 +0100

    Modelos 3D y simulación de físicas de tela con "muelles"

---
+++ Physics/PracticaTela/Assets/Source/P1/Spring.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring  {
+
+    #region InEditorVariables
+
+    public float Stiffness;
+    Node nodeA;
+    Node nodeB;
+
+    #endregion
+
+    public float Length0;
+    public float Length;
+
+    public MassSpringCloth Manager;
+
+    public  Spring(Node a, Node b, float dureza)
+    {
+        nodeA = a;
+        nodeB = b;
+        Length0 =  Length = (nodeA.ObtenerPos() - nodeB.ObtenerPos()).magnitude;
+        Stiffness = dureza;
+    }
+
+    public void ActualizarLongitud()
+    {
+        Length = (nodeA.ObtenerPos() - nodeB.ObtenerPos()).magnitude;
+    }
+
+    public void AddForces(float beta)
+    {
+        Length = (nodeA.ObtenerPos() - nodeB.ObtenerPos()).magnitude;
+        Vector3 ForceA = -Stiffness * (Length - Length0) * (nodeA.ObtenerPos() - nodeB.ObtenerPos()) / Length;
+        Vector3 vectorVelocidad = nodeA.Vel - nodeB.Vel;
+        Vector3 vectorDirector = (nodeA.ObtenerPos() - nodeB.ObtenerPos()).normalized;
+        Vector3 ForceB =  -beta * Stiffness * Vector3.Dot(vectorVelocidad , vectorDirector) * vectorDirector;
+        nodeA.AddForce(ForceA+ForceB);
+        nodeB.AddForce(-ForceA-ForceB);
+    }
+}
